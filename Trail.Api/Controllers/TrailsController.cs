0508a6adbd9db47a8ea318dab354270af5696b8d
mxs2 <mat.xaviersantos@gmail.com>
2026-05-28 17:55:24 -0300

    feat(trails): add roles and response metadata

--- Trail.Api/Controllers/TrailsController.cs
+++ Trail.Api/Controllers/TrailsController.cs
@@ -7,14 +7,22 @@ namespace Trail.Api.Controllers;
 
 [ApiController]
 [Route("trails")]
-[Authorize]
+[Authorize(Roles = "Student,Mentor,Manager")]
 public class TrailsController(TrailService trailService) : ControllerBase
 {
     [HttpGet]
+    [ProducesResponseType(typeof(IReadOnlyList<TrailResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     public async Task<ActionResult<IReadOnlyList<TrailResponse>>> List(CancellationToken ct)
         => Ok(await trailService.ListAsync(ct));
 
     [HttpGet("{id:guid}/challenges")]
+    [ProducesResponseType(typeof(IReadOnlyList<ChallengeResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<IReadOnlyList<ChallengeResponse>>> GetChallenges(Guid id, CancellationToken ct)
     {
         var challenges = await trailService.GetChallengesAsync(id, ct);
